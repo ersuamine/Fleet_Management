cbab241147f5b848c484ec51c924505a8371b53e
4745Mthn <metehan+metehan.ates@hotmail.com>
2024-07-09 18:42:27 -0400

    US01 Dashboard modules are verified

--- src/test/java/com/fleet_management/pages/US01_DashboardPage.java
+++ src/test/java/com/fleet_management/pages/US01_DashboardPage.java
@@ -1,7 +1,6 @@
 package com.fleet_management.pages;
 
 import com.fleet_management.utilities.Driver;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
