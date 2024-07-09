379282ddcc01c8e0f8b05527f7e5d6a778515368
ersuamine <amine.ersu@gmail.com>
2024-07-09 15:03:08 -0400

    Project setup

---
+++ src/test/java/com/fleet_management/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.fleet_management.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/fleet_management/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
