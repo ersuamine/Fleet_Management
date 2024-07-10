6b4b285f6e67eb16bbcede9122c2ef008a2364d2
zeynepcakm <zynpckmk8@gmail.com>
2024-07-10 16:53:00 -0400

    US03-AC1 completed

---
+++ src/test/java/com/fleet_management/pages/PinbarHelp_page_ZC.java
@@ -0,0 +1,25 @@
+package com.fleet_management.pages;
+
+import com.fleet_management.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PinbarHelp_page_ZC extends BasePage{
+
+    public PinbarHelp_page_ZC(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
+    @FindBy(xpath = "//div/a[@href='/pinbar/help']")
+    public WebElement howToUseThisSpace;
+
+
+    @FindBy(xpath = "//h3[.='How To Use Pinbar']")
+    public WebElement howToUsePinbar;
+
+
+    @FindBy(xpath = "//div[@class='clearfix']/p[1]")
+    public WebElement usePinIconText;
+}
---
+++ src/test/java/com/fleet_management/step_definitions/Pinbar_stepDef_ZC.java
@@ -0,0 +1,48 @@
+package com.fleet_management.step_definitions;
+
+import com.fleet_management.pages.PinbarHelp_page_ZC;
+import com.fleet_management.utilities.BrowserUtils;
+import com.fleet_management.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pinbar_stepDef_ZC {
+
+    PinbarHelp_page_ZC pinbarHelpPageZc = new PinbarHelp_page_ZC();
+    LoginStepDefs loginStepDefs = new LoginStepDefs();
+
+    @When("{string} click the “Learn how to use this space” link on the homepage")
+    public void clickTheLearnHowToUseThisSpaceLinkOnTheHomepage(String userType) {
+
+        loginStepDefs.the_user_logged_in_as(userType);
+        pinbarHelpPageZc.howToUseThisSpace.click();
+
+    }
+
+
+
+    @Then("{string} see “How To Use Pinbar”")
+    public void seeHowToUsePinbar(String userType) {
+
+        String actualPinbarText = pinbarHelpPageZc.howToUsePinbar.getText();
+
+        Assert.assertEquals("How To Use Pinbar", actualPinbarText);
+    }
+
+
+
+    @And("{string} see “Use pin icon on the right top corner of page to create fast access link in the pinbar.”")
+    public void seeUseThePinIconOnTheRightTopCornerOfPageToCreateFastAccessLinkInThePinbar(String userType) {
+
+        String actualText = pinbarHelpPageZc.usePinIconText.getText();
+        String expectedText = "Use pin icon on the right top corner of page to create fast access link in the pinbar.";
+
+        Assert.assertTrue(actualText.contains(expectedText));
+    }
+}
