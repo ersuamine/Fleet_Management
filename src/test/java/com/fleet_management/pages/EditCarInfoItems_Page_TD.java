084094163265b7ba93b711dbd9beb0810d788168
dilovar1992 <dellovar56@gmail.com>
2024-07-11 15:12:46 -0400

    US6 page was created

--- src/test/java/com/fleet_management/pages/EditCarInfoItems_Page_TD.java
+++ src/test/java/com/fleet_management/pages/EditCarInfoItems_Page_TD.java
@@ -1,10 +1,26 @@
 package com.fleet_management.pages;
 
 import com.fleet_management.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class EditCarInfoItems_Page_TD extends BasePage {
     public EditCarInfoItems_Page_TD() {
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//a[.='Vehicles']")
+    public WebElement Vehicles;
+
+    @FindBy(xpath = "//li[@class='dropdown dropdown-level-1'][1]")
+    public WebElement FleetModule;
+
+    @FindBy(xpath = "//a[@class='dropdown-toggle']")
+    public WebElement Dots;
+    //(//a[@title='Delete'])[1]
+    @FindBy(xpath = "(//ul[@class='nav nav-pills icons-holder launchers-list']//a)")
+    public List<WebElement> dotOptions;
 }
