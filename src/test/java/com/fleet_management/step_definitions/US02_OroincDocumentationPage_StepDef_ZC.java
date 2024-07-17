2dd07ddb584758f1576258b77a3f4aa356763eb7
zeynepcakm <zynpckmk8@gmail.com>
2024-07-16 20:05:08 -0400

    US02 step def class is created

---
+++ src/test/java/com/fleet_management/step_definitions/US02_OroincDocumentationPage_StepDef_ZC.java
@@ -0,0 +1,19 @@
+package com.fleet_management.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US02_OroincDocumentationPage_StepDef_ZC {
+
+    @When("users click the the question mark icon")
+    public void users_click_the_the_question_mark_icon() {
+
+    }
+
+
+
+    @Then("users access to the Oroinc Documentation page")
+    public void users_access_to_the_oroinc_documentation_page() {
+
+    }
+}
